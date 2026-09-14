f2adab8c59e264e97cbb8b12a0e9614d74b25f04
almash591-sudo <almash591@gmail.com>
2026-09-14 22:29:05 +0300

    Update page.tsx

--- app/exam/page.tsx
+++ app/exam/page.tsx
@@ -17,8 +17,8 @@ export default async function ExamPage() {
 
   return (
     <div className="container">
-      <Link href="/" className="link">
-        &larr; Назад
+      <Link href="/" className="btn btn-outline" style={{ marginBottom: '16px' }}>
+        ← Назад
       </Link>
       <h1 className="wordmark" style={{ fontSize: '24px' }}>
         Экзамен
